45210249aad996c0b0fa29b43affc4d37189e26e
Lucas Kacher <lucaskacher@gmail.com>
2024-08-04 06:45:43 -1000

    feat: reorg (#5)

---
+++ cmd/cli/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/promiseofcake/artifactsmmo-engine/internal/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/promiseofcake/artifactsmmo-engine/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
