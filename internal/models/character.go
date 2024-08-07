4604fc9dd20188cb02ec995af453c26f22d4284c
Lucas Kacher <lucaskacher@gmail.com>
2024-08-06 23:21:07 -0700

    feat: allow angine actions to wait until ready

--- internal/models/character.go
+++ internal/models/character.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/promiseofcake/artifactsmmo-go-client/client"
 )
@@ -20,6 +22,16 @@ func (c Character) CountInventory() int {
 		count += item.Quantity
 	}
 	return count
+}
+
+// GetCooldownDuration returns the time.Duration remaining on the character for cooldown
+func (c Character) GetCooldownDuration() (time.Duration, error) {
+	t, err := c.CooldownExpiration.AsCharacterSchemaCooldownExpiration0()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get cooldown expiration: %w", err)
+	}
+
+	return time.Until(t), nil
 }
 
 // GetPosition returns the given Coordinates for the Character in question
